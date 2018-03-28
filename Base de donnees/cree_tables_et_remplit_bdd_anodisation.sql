e175b73e39d37e7652c026af7782bda3bb91b3e4
nairo5913 <florian.provost59@gmail.com>
2018-03-28 12:18:51 +0200

    - Script SQL
    - Modification de la structure des tables de la BdD
    - Ajout de fonctionalité en commentaire en attendant les fichiers csv pour remplir la table

--- Base de donnees/cree_tables_et_remplit_bdd_anodisation.sql
+++ Base de donnees/cree_tables_et_remplit_bdd_anodisation.sql
@@ -1,47 +1,67 @@
 CREATE TABLE "Processus" (
-    id integer,
-    PRIMARY KEY (id),
-    nom char(255) NOT NULL,
+    id_processus integer,
+    PRIMARY KEY (id_processus),
+    nom_processus char(255) NOT NULL,
     processus_valide boolean DEFAULT FALSE,
     duree_mouvement time without time zone NOT NULL,
     numero_bain integer NOT NULL,
     duree_bain time without time zone NOT NULL
 );
 
-/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'un processus';*/
-
-CREATE TABLE "Mouvements" (
-    id integer,
-    PRIMARY KEY (id),
-    nom char(255) NOT NULL,
-    id_processus integer,
-    nb_processus_utilise integer,
-    axe char(1)
-/*    partie_bras interger ou text ou char*/
-);
-
-/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'un mouvement';*/
+/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'un processus';
+COPY "Processus" FROM './donnees_processus.csv' WITH DELIMITER ',';*/
 
 CREATE TABLE "Trajectoires" (
-    id integer,
-    PRIMARY KEY (id),
-    nom char(255) NOT NULL,
+    id_trajectoire integer,
+    PRIMARY KEY (id_trajectoire),
+    nom_trajectoire char(255) NOT NULL,
     id_processus integer,
     id_mouvements integer,
     contenu_trajectoire text,
-    axe char(1)
+    axe char
 );
 
-/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'une trajectoire';*/
+/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'une trajectoire';
+COPY "Trajectoires" FROM './donnees_trajectoires.csv' WITH DELIMITER ',';*/
 
-CREATE TABLE "Intermediaire_processus_mouvements" (
-    nom_champ char
+CREATE TABLE "Mouvements" (
+    id_mouvements integer,
+    PRIMARY KEY (id_mouvements),
+    nom_mouvement char(255) NOT NULL,
+    id_processus integer,
+    contenu_mouvement text,
+    nb_processus_utilise integer,
+    axe char,
+/*    partie_bras integer ou text ou char,*/
+    vitesse char,
+    temps_attente time without time zone
+    
 );
 
-/*COMMENT ON TABLE "Processus" IS 'Table intermediaire pour faire la liaison entre la table "Processus" et "Mouvements"';*/
+/*COMMENT ON TABLE "Processus" IS 'Contient les details d\'un mouvement';
+COPY "Mouvements" FROM './donnees_mouvements.csv' WITH DELIMITER ',';*/
+
+CREATE TABLE "Intermediaire_processus_trajectoires" (
+    /*nom_champ char,*/
+    id_p integer,
+    id_t integer,
+    PRIMARY KEY (id_p, id_t),
+    ordre_trajectoires text
+);
+
+/*COMMENT ON TABLE "Processus" IS 'Table intermediaire pour faire la liaison entre la table "Processus" et "Trajectoires"';
+COPY "Intermediaire_processus_trajectoires" FROM './donnees_intermediaire_processus_trajectoires.csv' WITH DELIMITER ',';*/
 
 CREATE TABLE "Intermediaire_mouvements_trajectoires" (
-    nom_champ char
+    /*nom_champ char,*/
+    id_t integer,
+    id_m integer,
+    PRIMARY KEY (id_m, id_t),
+    
+    ordre_mouvements text,
+    point_depart char,
+    point_arrive char
 );
 
-/*COMMENT ON TABLE "Processus" IS 'Table intermediaire pour faire la liaison entre la table "Mouvements" et "Trajectoires"';*/
+/*COMMENT ON TABLE "Processus" IS 'Table intermediaire pour faire la liaison entre la table "Mouvements" et "Trajectoires"';
+COPY "Intermediaire_mouvements_trajectoires" FROM './donnees_intermediaire_mouvements_trajectoires.csv' WITH DELIMITER ',';*/
