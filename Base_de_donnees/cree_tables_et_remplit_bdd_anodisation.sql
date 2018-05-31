29b8cfd2299995f15e64e32a46889aeaf82fbda3
nairo5913 <florian.provost59@gmail.com>
2018-06-01 00:59:14 +0200

    - Modification de la structure de la base de données  - Modification des données de remplissage  - Les fichiers CSV pour remplir la base de données sont déplacer dans un dossier Donnees  - Ajout d'un diagramme représentant la structure de la Base de données

--- Base_de_donnees/cree_tables_et_remplit_bdd_anodisation.sql
+++ Base_de_donnees/cree_tables_et_remplit_bdd_anodisation.sql
@@ -3,26 +3,29 @@ CREATE TABLE "processus" (
     nom_processus char(100) NOT NULL,
     processus_valide boolean DEFAULT FALSE,
     duree_processus time without time zone NOT NULL,
+    nombre_bain integer,
     PRIMARY KEY (id_processus)
 ) WITHOUT OIDS;
 
 COMMENT ON TABLE "processus" IS 'Contient les détails des processus';
-\COPY "processus" FROM './donnees_processus.csv' WITH DELIMITER ',';
+\COPY processus (nom_processus, processus_valide, duree_processus) FROM './Donnees/donnees_processus.csv' WITH DELIMITER ',';
 
 CREATE TABLE "trajectoires" (
     id_trajectoire SERIAL,
     nom_trajectoire char(100) NOT NULL,
+    trajectoire_valide boolean DEFAULT FALSE,
     contenu_trajectoire text,
     duree_trajectoire time without time zone,
     PRIMARY KEY (id_trajectoire)
 ) WITHOUT OIDS;
 
 COMMENT ON TABLE "trajectoires" IS 'Contient les détails des trajectoire';
-\COPY "trajectoires" FROM './donnees_trajectoires.csv' WITH DELIMITER ',';
+\COPY trajectoires (nom_trajectoire, contenu_trajectoire, duree_trajectoire) FROM './Donnees/donnees_trajectoires.csv' WITH DELIMITER ',';
 
 CREATE TABLE "mouvements" (
     id_mouvement SERIAL,
     nom_mouvement char(100) NOT NULL,
+    mouvement_valide boolean DEFAULT FALSE,
     contenu_mouvement text,
     axe_x integer,
     axe_y integer,
@@ -35,7 +38,7 @@ CREATE TABLE "mouvements" (
 ) WITHOUT OIDS;
 
 COMMENT ON TABLE "mouvements" IS 'Contient les détails des mouvement';
-\COPY "mouvements" FROM './donnees_mouvements.csv' WITH DELIMITER ',';
+\COPY mouvements (nom_mouvement, contenu_mouvement, axe_x, axe_y, axe_z, vitesse, temps_attente, duree_mouvement) FROM './Donnees/donnees_mouvements.csv' WITH DELIMITER ',';
 
 CREATE TABLE "intermediaire_processus_trajectoires" (
     /*nom_champ char,*/
@@ -48,7 +51,7 @@ CREATE TABLE "intermediaire_processus_trajectoires" (
 );
 
 COMMENT ON TABLE "intermediaire_processus_trajectoires" IS 'Table intermediaire pour faire la liaison entre la table "processus" et "trajectoires"';
-\COPY "intermediaire_processus_trajectoires" FROM './donnees_intermediaire_processus_trajectoires.csv' WITH DELIMITER ',';
+\COPY "intermediaire_processus_trajectoires" FROM './Donnees/donnees_intermediaire_processus_trajectoires.csv' WITH DELIMITER ',';
 
 CREATE TABLE "intermediaire_mouvements_trajectoires" (
     /*nom_champ char,*/
@@ -61,4 +64,4 @@ CREATE TABLE "intermediaire_mouvements_trajectoires" (
 );
 
 COMMENT ON TABLE "intermediaire_mouvements_trajectoires" IS 'Table intermediaire pour faire la liaison entre la table "mouvements" et "trajectoires"';
-\COPY "intermediaire_mouvements_trajectoires" FROM './donnees_intermediaire_mouvements_trajectoires.csv' WITH DELIMITER ',';
+\COPY "intermediaire_mouvements_trajectoires" FROM './Donnees/donnees_intermediaire_mouvements_trajectoires.csv' WITH DELIMITER ',';
